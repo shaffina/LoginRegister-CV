73a018207b534df38cb0c8b0b238cf6e699ac459
chvcyou <shaffinadw@gmail.com>
2023-10-24 15:06:16 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginRegisterController;
+
+
+Route::controller(LoginRegisterController::class)->group(function() {
+ Route::get('/register', 'register')->name('register');
+ Route::post('/store', 'store')->name('store');
+ Route::get('/login', 'login')->name('login');
+ Route::post('/authenticate', 'authenticate')->name('authenticate');
+ Route::get('/dashboard', 'dashboard')->name('dashboard');
+ Route::post('/logout', 'logout')->name('logout');
+});
